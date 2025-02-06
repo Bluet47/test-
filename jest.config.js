61f158dd7d8173659931b9728db1289983f7fdb1
Bluet47 <treasure.k.muriithi@gmail.com>
2025-02-06 16:58:33 +0000

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 export default {
   transform: {},
-  extensionsToTreatAsEsm: [".js"],
   testEnvironment: "node",
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1" // Fix module resolution for ES Modules
   }
 };
+
 
